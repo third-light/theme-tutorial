80126fd8cd42a74e8a85bbfd05473d3cb018dbb5
matarc <hilaire.mathieu@gmail.com>
2019-10-08 12:08:46 +0200

    click action on the right button

--- js/likes.js
+++ js/likes.js
@@ -41,7 +41,7 @@ $(document).ready(function(){
     })
 	getLikes(assetId);
 	
-	$("#resetLikes").click(function(){
+	$("#reset_button").click(function(){
 	    assetId = $("#reset_button").val();
 		resetLikes(assetId);
 	})
