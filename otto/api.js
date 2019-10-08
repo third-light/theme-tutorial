2b3380eae3b49b49ad51c83f9349212678d5cec9
matarc <hilaire.mathieu@gmail.com>
2019-10-08 12:05:48 +0200

    debug

--- otto/api.js
+++ otto/api.js
@@ -18,9 +18,11 @@ switch (scope.ApiRequest.Action) {
     case "resetLikes":
         if (typeof scope.userDetails == 'undefined')
         {
+            console.log("userDetails is undefined")
             scope.ApiError = "resetLikes can only be used by authenticated users"
             break
         }
+        console.log("userDetails is defined")
         try {
             res = store.Delete("assets.likes." + scope.ApiRequest.Params.assetId)
             scope.ApiResponse = res
